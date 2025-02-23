c7fc2c1cd71e48e8971e8dea4e75c79a61d6533f
wen2ms <1819509111@qq.com>
2025-02-23 19:10:32 +0800

    Feat: implement BubbleSort class

--- qthread_sort/mythread.cpp
+++ qthread_sort/mythread.cpp
@@ -27,3 +27,32 @@ void RandomGeneration::run() {
     
     emit send_random_numbers(random_numbers);
 }
+
+BubbleSort::BubbleSort(QObject *parent) : QThread{parent} {}
+
+void BubbleSort::recv_random_numbers(const QVector<int>& random_numbers) {
+    sorted_numbers_ = random_numbers;
+}
+
+void BubbleSort::run() {
+    qDebug() << "The thread addree of bubble sort:" << QThread::currentThread();
+    
+    QElapsedTimer sort_timer;
+    
+    sort_timer.start();
+    
+    int temp_value;
+    for (int i = 0; i < sorted_numbers_.size(); ++i) {
+        for (int j = 0; j < sorted_numbers_.size() - i -1; ++j) {
+            if (sorted_numbers_[j] > sorted_numbers_[j + 1]) {
+                temp_value = sorted_numbers_[j];
+                sorted_numbers_[j] = sorted_numbers_[j + 1];
+                sorted_numbers_[j + 1] = temp_value;
+            }
+        }
+    }
+    
+    qDebug() << "Total time to bubble sort:" << sort_timer.elapsed();
+    
+    emit send_sorted_numbers(sorted_numbers_);
+}
--- qthread_sort/mythread.h
+++ qthread_sort/mythread.h
@@ -22,4 +22,21 @@ class RandomGeneration : public QThread {
     int numbers_count_;
 };
 
+class BubbleSort : public QThread {
+    Q_OBJECT
+  public:
+    explicit BubbleSort(QObject *parent = nullptr);
+    
+  public slots:
+    void recv_random_numbers(const QVector<int>& random_numbers);
+    
+  protected:
+    void run() override;
+    
+  signals:
+    void send_sorted_numbers(QVector<int> sorted_nubmers);
+    
+  private:
+    QVector<int> sorted_numbers_;
+};
 #endif  // MYTHREAD_H
